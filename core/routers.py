0fb0abeabe7ae70d2a0e5b4f3fde198b0fac2652
vivek <vivekthakur8800@gmail.com>
2024-09-20 23:35:41 +0530

    crud API's

--- core/routers.py
+++ core/routers.py
@@ -1,10 +1,23 @@
+# from django.urls import path
+# from .views import StudentCreateView,StudentListView,StudentDetailView,StudentUpdateView,StudentDeleteView
+# app_name="core"
+# urlpatterns=[
+#     path("",StudentListView.as_view(),name="students"),
+#     path("detail/<int:pk>",StudentDetailView.as_view(),name="student_detail"),
+#     path("update/<int:pk>",StudentUpdateView.as_view(),name="student_update"),
+#     path("add_student",StudentCreateView.as_view(),name="student_create"),
+#     path("delete/<int:pk>",StudentDeleteView.as_view(),name="delete")
+# ]
+
+##########################################################################
+from rest_framework import routers
 from django.urls import path
-from .views import StudentCreateView,StudentListView,StudentDetailView,StudentUpdateView,StudentDeleteView
-app_name="core"
-urlpatterns=[
-    path("",StudentListView.as_view(),name="students"),
-    path("detail/<int:pk>",StudentDetailView.as_view(),name="student_detail"),
-    path("update/<int:pk>",StudentUpdateView.as_view(),name="student_update"),
-    path("add_student",StudentCreateView.as_view(),name="student_create"),
-    path("delete/<int:pk>",StudentDeleteView.as_view(),name="delete")
+from .views import StudentView
+router=routers.DefaultRouter(trailing_slash=False)
+
+# router.register("students",StudentView,basename="students")
+urlpatterns=router.urls
+urlpatterns+=[
+    path("students",StudentView.as_view(),name="students"),
+    path("students/<int:pk>",StudentView.as_view(),name="students"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,31 +1,83 @@
-from django.shortcuts import render,redirect
-# from django.views.generic import 
-from django.urls import reverse
+# from django.shortcuts import render,redirect
+# # from django.views.generic import 
+# from django.urls import reverse
+# from .models import Student
+# from django.views.generic.edit import CreateView,UpdateView,DeleteView
+# from django.views.generic.list import ListView
+# from django.views.generic.detail import DetailView
+# # Create your views here.
+# class StudentCreateView(CreateView):
+#     model=Student
+#     fields=['name','roll_no']
+#     success_url="/"
+
+# class StudentListView(ListView):
+#     model=Student
+#     # fields=['name','roll_no']
+
+# class StudentDetailView(DetailView):
+#     model = Student
+
+# class StudentUpdateView(UpdateView):
+#     model=Student
+#     fields=['name','roll_no']
+#     success_url="/"
+
+#     def get_success_url(self):
+#         return reverse("core:students")
+
+# class StudentDeleteView(DeleteView):
+#     model=Student
+#     success_url='/'
+##########################################################################
+
+from rest_framework import viewsets
+from .serializers import StudentSerializer
 from .models import Student
-from django.views.generic.edit import CreateView,UpdateView,DeleteView
-from django.views.generic.list import ListView
-from django.views.generic.detail import DetailView
-# Create your views here.
-class StudentCreateView(CreateView):
-    model=Student
-    fields=['name','roll_no']
-    success_url="/"
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+# class StudentView(viewsets.ModelViewSet):
+#     queryset=Student.objects.all()
+#     serializer_class=StudentSerializer
 
-class StudentListView(ListView):
-    model=Student
-    # fields=['name','roll_no']
-
-class StudentDetailView(DetailView):
-    model = Student
-
-class StudentUpdateView(UpdateView):
-    model=Student
-    fields=['name','roll_no']
-    success_url="/"
-
-    def get_success_url(self):
-        return reverse("core:students")
-
-class StudentDeleteView(DeleteView):
-    model=Student
-    success_url='/'
+class StudentView(APIView):
+    def get(self,request,*args,**kwargs):
+        pk=kwargs.get('pk')
+        if pk:
+            queryset=get_object_or_404(Student,id=pk)
+            serializer=StudentSerializer(queryset)
+        else:
+            queryset=Student.objects.all()
+            serializer=StudentSerializer(queryset,many=True)
+        return Response(serializer.data)
+    
+    def post(self,request,*args,**kwargs):
+        serializer=StudentSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(data=serializer.data)
+        return Response(serializer.errors)
+    
+    def put(self,request,*args,**kwargs):
+        pk=kwargs.get("pk")
+        queryset=get_object_or_404(Student,id=pk)
+        serializer=StudentSerializer(queryset,data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    def patch(self,request,*args,**kwargs):
+        pk=kwargs.get('pk')
+        queryset=get_object_or_404(Student,id=pk)
+        serializer=StudentSerializer(queryset,data=request.data,partial=True)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(data=serializer.data)
+        return Response(serializer.errors)
+    
+    def delete(self,request,*args,**kwargs):
+        pk=kwargs.get('pk')
+        queryset=get_object_or_404(Student,id=pk)
+        queryset.delete()
+        return Response({"data":"deleted successfully"})
